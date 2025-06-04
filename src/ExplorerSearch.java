3df08dc9d76e3006c896e678d6bbced24c24370c
SketchRudy <55453823+SketchRudy@users.noreply.github.com>
2025-06-04 13:26:56 -0700

    Refactor reachableArea and reachableAreaHelper methods for improved boundary checks and visited tracking

--- src/ExplorerSearch.java
+++ src/ExplorerSearch.java
@@ -32,14 +32,34 @@ public class ExplorerSearch {
         // Implement your method here!
         // Please also make more test cases
         // I STRONGLY RECOMMEND testing some helpers you might make too
+
+        boolean[][] visited = new boolean[island.length][island[0].length];
         return -1;
     }
 
     public static int reachableAreaHelper(int[][] island, boolean[][] visited, int row, int col) {
-        if (row > island.length || col > island[0].length || row != 0 || col != 0) {
+        // out of bounds
+        if (row >= island.length || row < 0 || col < 0 || col >= island[0].length) {
             return 0;
         }
-        return 0; 
+        
+        // if it's water or mountain
+        if (island[row][col] == 2 || island[row][col] == 3) {
+            return 0;
+        }
+
+        // if it's already visited
+        if (visited[row][col]) {
+            return 0;
+        }
+
+        // add it to visited
+        visited[row][col] = true;
+
+        int count = 1;
+        count += reachableAreaHelper(island, visited, row, col);
+
+        return count; 
     }
 
     public static int[] findStart(int[][] island) {
